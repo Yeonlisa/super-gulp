1ecaa3bc3e48b947d9e1946495884ed3213f7d0f
yeonlisa <tkzw4869@gmail.com>
2022-03-22 15:36:41 +0900

    Pug Task part One

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,19 @@
 const gulp = require("gulp");
+import gpug from "gulp-pug";
+
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest: "build"
+    }
+};
+
+export const pug = () =>
+    gulp
+        .src(routes.pug.src)
+        .pipe(gpug())
+        .pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([pug]);
+
+
